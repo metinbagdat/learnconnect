3c1dcbf0ab05f41a55b8048d8d883d06438a5bc4
Metin Bagdat <metinbagdat@users.noreply.github.com>
2026-01-17 21:50:09 +0300

    Remove old project files - keep only clean structure

--- client/src/pages/control-panel.tsx
+++
@@ -1,9 +0,0 @@
-import { StudyPlannerControlPanel } from "@/components/study-planner-control-panel";
-
-export default function ControlPanel() {
-  return (
-    <div className="min-h-screen bg-background">
-      <StudyPlannerControlPanel />
-    </div>
-  );
-}
