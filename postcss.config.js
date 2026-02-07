2eca13b00c51be2dea9656037671650bb1cb91c6
CI Test Runner <actions@users.noreply.github.com>
2026-02-07 13:26:10 +0000

    fix: simplify postcss config to resolve build error

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,6 @@
-function removeWebkitTextSizeAdjust() {
-  return {
-    postcssPlugin: "remove-webkit-text-size-adjust",
-    Declaration(decl) {
-      if (decl.prop === "-webkit-text-size-adjust") decl.remove();
-    },
-  };
-}
-removeWebkitTextSizeAdjust.postcss = true;
-
-export default {
+module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    removeWebkitTextSizeAdjust,
   },
 };
